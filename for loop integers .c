5cce91a862aa5d6db3624250073e5e44b8b0231a
Muchkev <muchokikelvin62@gmail.com>
2024-10-10 23:11:27 +0300

    Add files via upload

---
+++ for loop integers .c
@@ -0,0 +1,21 @@
+/*for loop-outnumbers to user friendly 
+Registration number:CT101/G/25008/24
+Author:Kelvin Muchoki
+Date:10/10/2024
+*/
+#include<stdio.h>
+
+int main()
+{
+int i;
+int count;
+
+printf("Enter integers you would like to print:",count);
+scanf("%d",&count);
+
+for(i=1;i<=count;i++){
+printf("\n%d",i);
+}
+
+    return 0;
+}
