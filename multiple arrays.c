f73a40bc8556b0e6057487fc1e2408a64a6a4dc5
Muchkev <muchokikelvin62@gmail.com>
2024-10-17 18:00:25 +0300

    Add files via upload

---
+++ multiple arrays.c
@@ -0,0 +1,14 @@
+
+//arrays-id arrays
+#include<stdio.h>
+
+int main()
+{
+int i;
+int marks[10]={1,2,3,4,5,6,7,8,9,10};
+
+for(i=0;i<10;i++){
+printf("marks [%d]=%d\n",i,marks[i]);
+}
+    return 0;
+}
